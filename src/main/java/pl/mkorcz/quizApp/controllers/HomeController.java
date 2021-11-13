e1715933a2844bfc809399e42ace8535b9bee6b0
Michal <mkorcz01@gmail.com>
2021-11-13 22:03:08 +0100

    added quiz postmapping and quiz.html

--- src/main/java/pl/mkorcz/quizApp/controllers/HomeController.java
+++ src/main/java/pl/mkorcz/quizApp/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package pl.mkorcz.quizApp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -10,5 +14,15 @@ public class HomeController {
     public String home() {
         return "index.html";
     }
+
+    @PostMapping("/quiz")
+    public String quiz(@RequestParam String username, Model model, RedirectAttributes redirectAttributes) {
+        if(username.equals("")) {
+            redirectAttributes.addFlashAttribute("warning", "You must enter your name");
+            return "redirect:/";
+        }
+        return "quiz.html";
+    }
+
 
 }
