887c4b74e929ce0de99030204a10cc4221ac2eed
Michal <mkorcz01@gmail.com>
2021-11-13 23:55:53 +0100

    added entity Question

---
+++ src/main/java/pl/mkorcz/quizApp/model/Question.java
@@ -0,0 +1,103 @@
+package pl.mkorcz.quizApp.model;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Component
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int questionId;
+    private String title;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private int answer;
+    private int choice;
+
+    public Question() {
+    }
+
+    public Question(int questionId, String title, String optionA, String optionB, String optionC, int answer, int choice) {
+        this.questionId = questionId;
+        this.title = title;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.answer = answer;
+        this.choice = choice;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public int getChoice() {
+        return choice;
+    }
+
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "questionId=" + questionId +
+                ", title='" + title + '\'' +
+                ", optionA='" + optionA + '\'' +
+                ", optionB='" + optionB + '\'' +
+                ", optionC='" + optionC + '\'' +
+                ", answer=" + answer +
+                ", choice=" + choice +
+                '}';
+    }
+}
